7e2d4b17a5312b803f05e9878ee33d19598bd205
HOVis24 <giannangel29@gmail.com>
2022-11-05 00:00:02 +0800

    Update connection.php

--- controllers/connection.php
+++ controllers/connection.php
@@ -1,6 +1,6 @@
 <?php 
-$db = 'jeta'; //EVdW8x2a6w
-$con = new mysqli('localhost', 'root', '', $db);
+$db = 'sql12541627'; //EVdW8x2a6w
+$con = new mysqli('sql12.freemysqlhosting.net', 'sql12541627', 'IjwJjiTPmQ', $db);
 if ($con->connect_error) {
 	die("Connection failed: " . $con->connect_error);
 }
